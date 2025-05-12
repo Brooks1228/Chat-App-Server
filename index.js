e905284de0f97a5eaeab28019e25469cd38ecb75
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-05-12 08:39:30 -0400

    Chat App Server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected: ", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("message revieved: ", msg);
+    io.emit("message", msg); //broadcast to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected: ", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`✅ server running on port ${PORT}`);
+});
